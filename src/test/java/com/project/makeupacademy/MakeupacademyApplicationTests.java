84d234685f503f71a0a0b172a964d5b69996bcd2
sara-slavkovic <saraslavkovic3@gmail.com>
2025-01-12 23:42:28 +0100

    Initial commit

---
+++ src/test/java/com/project/makeupacademy/MakeupacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.makeupacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
